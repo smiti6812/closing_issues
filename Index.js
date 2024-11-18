7f94b690ed372a61871df6b460bf3bd698289511
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2024-11-18 20:47:09 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,35 +1,3 @@
-
-/*
-const core = require('@actions/core');
-const github = require('@actions/github');
-async function getIssuesFromPR() {
-  const token = core.getInput("token");
-  const owner = core.getInput("owner");
-  const repo = core.getInput("repo");
-  const pull_number = core.getInput("pull_number");  
-  const octokit = github.getOctokit(token);
-  const branch = core.getInput("branch");
-  const issue_number = branch.substring(0,branch.indexOf("-")) ;
- 
-  const issues = await octokit.rest.issues.get({
-        owner: owner,
-         repo: repo,    
-        issue_number: issue_number        
-      });
-    
-      const response = await octokit.rest.issues.update({
-        owner: owner,
-        ...github.context.repo,
-        state: 'closed',
-        issue_number: issue_number        
-      });
-        
-      core.setOutput("issue", issues.data);      
-}
-
-getIssuesFromPR().then(issues => console.log(issues));
-*/
-
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import {graphql} from "@octokit/graphql"
