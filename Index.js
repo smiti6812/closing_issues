5c3fa0dec17e4c96ac1db35f44a4194a065930ae
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2024-11-18 14:14:00 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -74,7 +74,7 @@ graphql(linkedIssuesQuery, {
   pull_number,
   maxIssues,
   headers: {
-    authorization:  `Bearer ${token}`,
+    authorization:  `${token}`,
   },
 })
 .then(result => console.log(result))
