485551ea9fa06cfbf283c3984f76ba696885525b
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2024-11-18 22:04:29 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -1,8 +1,6 @@
 import * as core from '@actions/core';
 import * as github from '@actions/github';
 import {graphql} from "@octokit/graphql"
-
-
 async function callLinkedIssuesQuery(){
 const linkedIssuesQuery = `
 query getLinkedIssues(
