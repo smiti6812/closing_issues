fe6cc90385b521aa9dc983bd291d97ded06fb07a
smiti6812 <73239938+smiti6812@users.noreply.github.com>
2025-01-08 13:07:21 +0100

    Update Index.js

--- Index.js
+++ Index.js
@@ -36,7 +36,7 @@ async function getMilestone(){
     {
         const milestone = octokit.request('GET /repos/{owner}/{repo}/milestones/{milestone_number}', {
           owner: owner,
-          repo: repo,
+          ...github.context.repo,
           milestone_number: 2,
           headers: {
             'X-GitHub-Api-Version': '2022-11-28'
